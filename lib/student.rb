bb4ba840f8c117e41b503c526d07b63f6918e0d7
James Johnson <jtaylorjohnson@gmail.com>
2020-05-02 23:23:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   def self.all
     sql = <<-SQL
       SELECT *
-      FROM songs 
+      FROM students 
     SQL
     
     DB[:conn].execute(sql).map do |row|
@@ -20,8 +20,31 @@ class Student
     end
       
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE grade = "9"
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end 
+    
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
     
   end
   
