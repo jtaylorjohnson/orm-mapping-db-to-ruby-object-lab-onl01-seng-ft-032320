2042e1cbc0825004e8e28fa1435f3ed74de2441c
James Johnson <jtaylorjohnson@gmail.com>
2020-05-02 23:34:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,18 @@ class Student
     end
   end 
   
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = grade
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end 
+  
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT *
